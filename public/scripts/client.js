298fd724c9e87303428146c639cd85d64688637d
Julianna <julianna.j.lim@gmail.com>
2020-08-12 16:14:54 -0400

    removed hardcoded object, added functions to get and post form request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 const renderTweets = function (tweets) {
   // loops through tweets
@@ -45,8 +45,7 @@ const createTweetElement = function (tweet) {
   let today = Date.now();
   let daysAgo = Math.round((today - createdAt) / 1000 / 60 / 60 / 24);
   console.log(daysAgo);
-  let $tweet = /* Your code for creating the tweet element */
-    // ...
+  let $tweet =
     `<article class="tweet-box">
       <header>
       <div class='tweet-header'>
@@ -67,11 +66,43 @@ const createTweetElement = function (tweet) {
           </div>
         </footer>
     </article>`
-
   return $tweet;
 }
 
+//create an AJAX get the tweets
 $(document).ready(function () {
-  renderTweets(data);
-})
+  const loadTweets = function () {
+    //gets json from /tweets
+    $.ajax({
+      url: "/tweets",
+      method: "GET"
+    }).then((response) => {
+      //empty tweet container section since renderTweets appends all of the tweets in our array
+      $('#tweet-box').empty();
+      renderTweets(response);
+    });
+  };
 
+  // Post request//
+
+  //load all our tweets upon loading page
+  loadTweets();
+  //each time we submit form (click tweet)
+  $('form').submit(function (event) {
+    //cancels original functionality since we want to add our own
+    event.preventDefault();
+    //encode a set of form elements as a string for submission.
+    const serialized = $(this).serialize();
+
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: serialized
+    }).then((response) => {
+      console.log("tweet posted");
+      loadTweets();
+      // turns the form blank
+      $('#tweettextID').val('');
+    });
+  });
+});
