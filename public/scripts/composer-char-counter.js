9e41d338c42f95c1bf1e2bfc60afdcc8f08b9972
Julianna <julianna.j.lim@gmail.com>
2020-08-12 21:13:06 -0400

    fixed some bugs and removed some comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,8 @@
 $(document).ready(function () {
-  // --- our code goes here ---
-  // console.log(this);
 
   $('#tweettextID').on('keyup', function () {
-    // console.log(this.value);
     let $counter = $(this).siblings('div').children('.counter');
-    // let numOfLetter = 140 - $counter.val().length;
-    // console.log(numOfLetter);
     $counter.text(`${140 - $(this).val().length}`);
-    // console.log($(this).val().length);
     if (Number($counter.text()) >= 0) {
       $counter.css('color', 'black');
     } else {
