1f4f135932b30c292fb270d2b70654f4f187e066
Julianna <julianna.j.lim@gmail.com>
2020-08-12 20:49:03 -0400

    fixed some bugs and cleaned up code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     // let numOfLetter = 140 - $counter.val().length;
     // console.log(numOfLetter);
     $counter.text(`${140 - $(this).val().length}`);
-    console.log($(this).val().length);
+    // console.log($(this).val().length);
     if (Number($counter.text()) >= 0) {
       $counter.css('color', 'black');
     } else {
